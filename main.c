958454894d859592dcceb3bb2383615450946475
Vicente Garcia Andrade <vicgarci@c3r10s6.42madrid.com>
2022-11-29 18:54:51 +0100

    abriendo_ventanas_p1

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: vicgarci <vicgarci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/29 11:54:19 by vicgarci          #+#    #+#             */
-/*   Updated: 2022/11/29 16:43:30 by vicgarci         ###   ########.fr       */
+/*   Updated: 2022/11/29 18:53:36 by vicgarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,15 @@
 
 int	main(void)
 {
-	if (!glfwInit())
+	if (glfwInit())
 	{
+		ft_printf("ABRO");
 		if (open_window())
 		{
 			ft_printf("HOLA");
+			while (1)
+				sleep(1);
 		}
 	}
+	glfwTerminate();
 }
--- open_window.c
+++ open_window.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Open_window.c                                      :+:      :+:    :+:   */
+/*   open_window.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vicgarci <vicgarci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/29 11:57:08 by vicgarci          #+#    #+#             */
-/*   Updated: 2022/11/29 11:57:38 by vicgarci         ###   ########.fr       */
+/*   Updated: 2022/11/29 18:42:46 by vicgarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,16 @@
 
 t_bool	open_window(void)
 {
+	GLFWwindow	*window;
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+	window = glfwCreateWindow(640, 480, "My Title", NULL, NULL);
+	if (!window)
+		return (false);
+	glfwMakeContextCurrent(window);
+	while (!glfwWindowShouldClose(window))
+	{
+	}
 	return (true);
 }
